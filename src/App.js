1ec4a1848c078688e63c2181d1af68581833b68c
gabrielestefono <gabrielestefono@hotmail.com>
2023-01-04 12:15:53 -0300

    04/01/2023 4º Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react"
-import { Text } from "react-native"
+import { View, Text } from "react-native"
 
-function App(){
-    return <Text>Primeiro Componente</Text>;
-}
 
-export default App
+import Primeiro from './components/Primeiro'
+import X, {Comp1, Comp2} from './components/Multi'
+export default () => (
+    <View>
+        <X />
+        <Comp1></Comp1>
+        <Comp2></Comp2>
+        <Primeiro></Primeiro>
+    </View>
+)
